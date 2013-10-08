14dc28ec8d2fe6f1756fe78d5d6960dceb1aaaf5
U-ASUS\Jayden_Lin <Jayden_Lin@Jayden_Lin-NB1.corpnet.asus>
2013-10-08 19:16:40 +0800

    first commit

---
+++ src/main/java/com/rest/test/TestRest.java
@@ -0,0 +1,21 @@
+package com.rest.test;  
+  
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.PathParam;  
+import javax.ws.rs.core.Response;  
+
+@Path("/test")  
+public class TestRest {  
+   
+ @GET  
+ @Path("/{name}")  
+ public Response getMsg(@PathParam("name") String name) {  
+   
+  String output = "Hello, " + name;  
+   
+  return Response.status(200).entity(output).build();  
+   
+ }  
+   
+}  
