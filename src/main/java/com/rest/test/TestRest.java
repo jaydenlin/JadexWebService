2ac17859193ac3e19c8ac0944f9adb265008400b
U-ASUS\Jayden_Lin <Jayden_Lin@Jayden_Lin-NB1.corpnet.asus>
2013-10-14 12:52:09 +0800

    add Jadex Platform start

--- src/main/java/com/rest/test/TestRest.java
+++ src/main/java/com/rest/test/TestRest.java
@@ -1,5 +1,10 @@
 package com.rest.test;  
   
+import jadex.base.Starter;
+import jadex.bridge.IExternalAccess;
+import jadex.commons.future.IFuture;
+import jadex.commons.future.ThreadSuspendable;
+
 import javax.ws.rs.GET;  
 import javax.ws.rs.Path;  
 import javax.ws.rs.PathParam;  
@@ -13,7 +18,16 @@ public class TestRest {
  public Response getMsg(@PathParam("name") String name) {  
    
   String output = "Hello, " + name;  
-   
+  String[] newargs =new String[]
+		  {
+		    "-welcome","false",
+		    "-cli","false",
+		    "-printpass","false"
+		  };
+		  IFuture<IExternalAccess> platfut = Starter.createPlatform(newargs);
+		  ThreadSuspendable sus =new ThreadSuspendable();
+		  IExternalAccess platform = platfut.get(sus);
+		  System.out.println("Started platform: "+platform.getComponentIdentifier());
   return Response.status(200).entity(output).build();  
    
  }  
